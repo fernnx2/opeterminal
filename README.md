b5242e766495339ff185b6b07ad4cd6b01bac9d0
fernando menjivar <fernandoaguilera_@outlook.es>
2019-10-21 17:48:57 -0600

    basic terminal for linux

---
+++ README.md
@@ -0,0 +1,6 @@
+#Shell Terminal, Sistemas Operativos
+
+## Instalacion
+
+    `make`
+    `./myshell`
---
+++ shell.c
@@ -0,0 +1,348 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
+#include <fcntl.h>
+#define BUFSIZE 1000
+#define INPBUF 100
+#define ARGMAX 10
+#define GREEN "\x1b[92m"
+#define BLUE "\x1b[94m"
+#define DEF "\x1B[0m"
+#define CYAN "\x1b[96m"
+
+struct _instr
+{
+    char * argval[INPBUF];
+    int argcount;
+};
+typedef struct _instr instruction;
+
+char *input,*input1;
+int exitflag = 0;
+int filepid,fd[2];
+char cwd[BUFSIZE];
+char* argval[ARGMAX]; // our local argc, argv
+int argcount = 0,inBackground = 0;
+int externalIn=0,externalOut=0;
+char inputfile[INPBUF],outputfile[INPBUF];
+void getInput();
+int function_exit();
+void function_pwd(char*, int);
+void function_clear();
+void function_ls();
+void nameFile(struct dirent*, char*);
+void executable();
+void pipe_dup(int, instruction*);
+void run_process(int, int, instruction*);
+/*stop process Ctrl+C*/
+void stopSignal()
+{
+    if(filepid!=0)
+    {
+        int temp = filepid;
+        kill(filepid, SIGINT);
+        filepid = 0;
+    }
+}
+
+/* input */
+void getInput()
+{
+    fflush(stdout);
+    input = NULL;
+    ssize_t buf = 0;
+    getline(&input,&buf,stdin);
+    input1 = (char *)malloc(strlen(input) * sizeof(char));
+    strncpy(input1,input,strlen(input));
+    argcount = 0;inBackground = 0;
+    while((argval[argcount] = strsep(&input, " \t\n")) != NULL && argcount < ARGMAX-1)
+    {
+        if(sizeof(argval[argcount])==0)
+        {
+            free(argval[argcount]);
+        }
+        else argcount++;
+        if(strcmp(argval[argcount-1],"&")==0)
+        {
+            inBackground = 1; //run in inBackground
+            return;
+        }
+    }
+    free(input);
+}
+/* Just a fancy name printing function*/
+void nameFile(struct dirent* name,char* followup)
+{
+    if(name->d_type == DT_REG)          // regular file
+    {
+        printf("%s%s%s",BLUE, name->d_name, followup);
+    }
+    else if(name->d_type == DT_DIR)    // a directory
+    {
+        printf("%s%s/%s",GREEN, name->d_name, followup);
+    }
+    else                              // unknown file types
+    {
+        printf("%s%s%s",CYAN, name->d_name, followup);
+    }
+}
+
+/* list content*/
+void function_ls()
+{
+    int i=0;
+    struct dirent **listr;
+    int listn = scandir(".", &listr, 0, alphasort);
+    if (listn >= 0)
+    {
+        printf("%s+--- Total %d objects in this directory\n",CYAN,listn-2);
+        for(i = 0; i < listn; i++ )
+        {
+            if(strcmp(listr[i]->d_name,".")==0 || strcmp(listr[i]->d_name,"..")==0)
+            {
+                continue;
+            }
+            else nameFile(listr[i],"    ");
+            if(i%8==0) printf("\n");
+        }
+        printf("\n");
+    }
+    else
+    {
+        perror ("+--- Error in ls ");
+    }
+
+}
+
+int main(int argc, char* argv[])
+{
+    signal(SIGINT,stopSignal);
+    int i;
+    int pipe1 = pipe(fd);
+    function_clear();
+    function_pwd(cwd,0);
+    while(exitflag==0)
+    {
+        externalIn = 0; externalOut = 0;inBackground = 0;
+        printf("%s%s ~> ",DEF,cwd ); //print user prompt
+        getInput();
+
+        if(strcmp(argval[0],"exit")==0 || strcmp(argval[0],"z")==0)
+        {
+            function_exit();
+        }
+        
+        else if(strcmp(argval[0],"pwd")==0 && !inBackground)
+        {
+            function_pwd(cwd,1);
+        }
+        else if(strcmp(argval[0],"clear")==0 && !inBackground)
+        {
+            function_clear();
+        }
+        else if(strcmp(argval[0],"ls")==0 && !inBackground)
+        {
+             function_ls();
+        }
+        
+        else
+        {
+            executable();
+        }
+
+    }
+}
+
+// run process
+void runprocess(char * cli, char* args[], int count)
+{
+  
+    int ret = execvp(cli, args);
+    char* pathm;
+    pathm = getenv("PATH");
+    char path[1000];
+    strcpy(path, pathm);
+    strcat(path,":");
+    strcat(path,cwd);
+    char * cmd = strtok(path, ":\r\n");
+    while(cmd!=NULL)
+    {
+       char loc_sort[1000];
+        strcpy(loc_sort, cmd);
+        strcat(loc_sort, "/");
+        strcat(loc_sort, cli);
+        printf("execvp : %s\n",loc_sort );
+        ret = execvp(loc_sort, args);
+        if(ret==-1)
+        {
+            perror("+--- Error in running executable ");
+            exit(0);
+        }
+        cmd = strtok(NULL, ":\r\n");
+    }
+}
+
+
+/* create */
+void pipe_dup(int n, instruction* command)
+{
+    int in = 0,fd[2], i;
+    int pid, status,pipest;
+
+    if(externalIn)
+    {
+        in = open(inputfile, O_RDONLY); 
+        if(in < 0)
+        {
+            perror("+--- Error in executable : input file ");
+        }
+    }
+   
+    for (i = 1; i < n; i++)
+    {
+      
+        pipe (fd);
+        int id = fork();
+        if (id==0)
+        {
+
+            if (in!=0)
+            {
+                dup2(in, 0);
+                close(in);
+            }
+            if (fd[1]!=1)
+            {
+                dup2(fd[1], 1);
+                close(fd[1]);
+            }
+
+            runprocess(command[i-1].argval[0], command[i-1].argval,command[i-1].argcount);
+            exit(0);
+
+        }
+        else wait(&pipest);
+        close(fd[1]);
+        in = fd[0];
+    }
+    i--; 
+    if(in != 0)
+    {
+        dup2(in, 0);
+    }
+    if(externalOut)
+    {
+        int ofd = open(outputfile, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        dup2(ofd, 1);
+    }
+    runprocess(command[i].argval[0], command[i].argval, command[i].argcount);
+}
+
+
+/* executables like ./a.out */
+void executable()
+{
+    instruction command[INPBUF];
+    int i=0,j=1,status;
+    char* curr = strsep(&input1," \t\n");
+                            
+    command[0].argval[0] = curr;
+
+    while(curr!=NULL)
+    {
+        curr = strsep(&input1, " \t\n");
+        if(curr==NULL)
+        {
+            command[i].argval[j++] = curr;
+        }
+        else if(strcmp(curr,"|")==0)
+        {
+            command[i].argval[j++] = NULL;
+            command[i].argcount = j;
+            j = 0;i++;
+        }
+        else if(strcmp(curr,"<")==0)
+        {
+            externalIn = 1;
+            curr = strsep(&input1, " \t\n");
+            strcpy(inputfile, curr);
+        }
+        else if(strcmp(curr,">")==0)
+        {
+            externalOut = 1;
+            curr = strsep(&input1, " \t\n");
+            strcpy(outputfile, curr);
+        }
+        else if(strcmp(curr, "&")==0)
+        {
+            inBackground = 1;
+        }
+        else
+        {
+            command[i].argval[j++] = curr;
+        }
+    }
+    command[i].argval[j++] = NULL; 
+    command[i].argcount = j;
+    i++;
+    filepid = fork();
+    if(filepid == 0)
+    {
+        pipe_dup(i, command);
+    }
+    else
+    {
+        if(inBackground==0)
+        {
+            waitpid(filepid,&status, 0);
+        }
+        else
+        {
+            printf("+--- Process running in inBackground. PID:%d\n",filepid);
+        }
+    }
+    filepid = 0;
+    free(input1);
+}
+
+
+
+/*exit*/
+int function_exit()
+{
+    exitflag = 1;
+    return 0; // return 0 to parent process in run.c
+}
+
+/* pwd*/
+void function_pwd(char* cwdstr,int command)
+{
+    char temp[BUFSIZE];
+    char* path=getcwd(temp, sizeof(temp));
+    if(path != NULL)
+    {
+        strcpy(cwdstr,temp);
+        if(command==1) 
+        {
+            printf("%s\n",cwdstr);
+        }
+    }
+    else perror("+--- Error in getcwd() : ");
+
+}
+
+/* clear the screen*/
+void function_clear()
+{
+    const char* blank = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO,blank,12);
+}
